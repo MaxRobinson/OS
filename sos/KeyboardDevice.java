2e64c1d5a8944ddb6905d1a53f1fc1dc27b9961b
Max Robinson <maxrgit@gmail.com>
2015-02-08 13:12:41 -0800

    Add Keyboard file
    Added keyboard file.

---
+++ sos/KeyboardDevice.java
@@ -0,0 +1,52 @@
+package sos;
+
+public class KeyboardDevice implements Device{
+    private int m_id;
+    
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public void setId(int id) {
+        m_id = id;
+    }
+
+    @Override
+    public boolean isSharable() {
+        return false;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public boolean isReadable() {
+        return true;
+    }
+
+    @Override
+    public boolean isWriteable() {
+        return false;
+    }
+    
+
+    /**
+     * read method
+     * return a different number every time something is read.
+     */
+    @Override
+    public int read(int addr) { 
+        return (int)(Math.random()*10000);
+    }
+
+    @Override
+    public void write(int addr, int data) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
